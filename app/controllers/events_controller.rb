909c82dda712be1936ebc60fe73bd15577bd8b6e
Maya Imhoff <mayelyimhoff@gmail.com>
2020-08-25 17:44:13 +0200

    added events index and show, added working paths

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,33 @@
 class EventsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.find(event_params)
+    @event.user_id = current_user.id
   end
 
   def update
+
   end
 
   def destroy
   end
+
+  private
+  def event_params
+    params.require(:event).permit(:category, :description, :location, :maximum_users, :date, :latitude, :longitude)
+  end
 end
+
