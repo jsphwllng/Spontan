30f78b3a27abbd61673239e143d317ec10d29026
marian heinzmann <faraskan@gmx.net>
2020-08-26 12:24:26 +0200

    changed event table column 'description' datatype from 'string' to 'text', also minor changes on the form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_160710) do
+ActiveRecord::Schema.define(version: 2020_08_26_100413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_08_25_160710) do
 
   create_table "events", force: :cascade do |t|
     t.string "category"
-    t.string "description"
+    t.text "description"
     t.string "location"
     t.integer "maximum_users"
     t.datetime "date"
