096b9d5dbed668d8a30bc3ded4fcd0523551359a
Maya Imhoff <mayelyimhoff@gmail.com>
2020-08-26 15:24:18 +0200

    simple search bar added

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,6 @@
 class MembersController < ApplicationController
  skip_before_action :authenticate_user!, only: :show
+
   def show
     @user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :members, only: [:show]
+  resources :members, only: [:show, :destroy]
   resources :events, only: [:new, :index, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
