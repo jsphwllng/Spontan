3ed768e096aca0c153320b9473a0c4654dfd366e
Maya Imhoff <mayelyimhoff@gmail.com>
2020-08-26 10:52:48 +0200

    index route again

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :members, only: [:show]
-  resources :events, only: [:new, :create, :show]
+  resources :events, only: [:new, :index, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
