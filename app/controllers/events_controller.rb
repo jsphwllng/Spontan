95d1f399c636b5bdb9a1418408b04eff3d90d886
Maya Imhoff <mayelyimhoff@gmail.com>
2020-08-27 15:11:58 +0200

    filter by location implemented

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,10 +7,12 @@ class EventsController < ApplicationController
     #   @movies = Movie.where(title: params[:query])
   def index
     if params[:query_event].present? || params[:query_category].present? || params[:query_location].present?
-      sql_query = "title ILIKE :query_event OR description ILIKE :query_event"
+        sql_query = "title ILIKE :query_event OR description ILIKE :query_event"
         sql_query_category = "category ILIKE :query_category"
         sql_query_location = "location ILIKE :query_location"
-        @events = Event.near(params[:query_location], 2)
+             # @user_location = current_user.location
+        @events = Event.near(current_user.location, params[:query_location].to_i)
+
       # @events= Event.where(sql_query_location, query_location: "%#{params[:query_location]}%")
       @events= Event.where(sql_query, query_event: "%#{params[:query_event]}%")
       @events = @events.where(sql_query_category, query_category: "%#{params[:query_category]}%")
