977bc8a2cc0d28f9e6083e17529796c074947185
Maya Imhoff <mayelyimhoff@gmail.com>
2020-08-27 16:22:52 +0200

    search perfected, update and destroy added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,26 +1,18 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
-
-
-    # if params[:query].present?
-    #   @movies = Movie.where(title: params[:query])
   def index
     if params[:query_event].present? || params[:query_category].present? || params[:query_location].present?
         sql_query = "title ILIKE :query_event OR description ILIKE :query_event"
         sql_query_category = "category ILIKE :query_category"
         sql_query_location = "location ILIKE :query_location"
-             # @user_location = current_user.location
-        @events = Event.near(current_user.location, params[:query_location].to_i)
-
-      # @events= Event.where(sql_query_location, query_location: "%#{params[:query_location]}%")
-      @events= Event.where(sql_query, query_event: "%#{params[:query_event]}%")
-      @events = @events.where(sql_query_category, query_category: "%#{params[:query_category]}%")
-
+        @events = Event.all
+        @events = Event.near(current_user.location, params[:query_location].to_i) if params[:query_location] != "0"
+        @events= @events.where(sql_query, query_event: "%#{params[:query_event]}%")
+        @events = @events.where(sql_query_category, query_category: "%#{params[:query_category]}%")
     else
       @events = Event.all
     end
-
 
     @markers = @events.map do |event|
       if event.date > DateTime.now
@@ -52,10 +44,17 @@ class EventsController < ApplicationController
   end
 
   def update
-
+    @event = Event.find(params[:id])
+      if @event.user_id == current_user.id
+      @event.update(event_params)
+      end
   end
 
   def destroy
+    @event = Event.find(params[:id])
+      if @event.user_id == current_user.id
+      @event.destroy
+      end
   end
 
   private
