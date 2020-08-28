a3f2f30a4c6d1a11324f2bf055352f996c6f72ea
joseph welling <joseph.welling@hotmail.com>
2020-08-28 12:26:57 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'date'
+require 'open-uri'
+
+food = URI.open('https://res.cloudinary.com/dbbmbbgju/image/upload/v1598521127/food_isnu2d.jpg')
+spiritual = URI.open('https://res.cloudinary.com/dbbmbbgju/image/upload/v1598521129/yoga_p096vn.jpg')
+political = URI.open('https://res.cloudinary.com/dbbmbbgju/image/upload/v1598605916/frnvwzs3f90klalc1731wmbjjhu0.jpg')
+sports = URI.open('https://res.cloudinary.com/dbbmbbgju/image/upload/v1598606008/rku4jcqqaigdp6yhi9vbfpsg231k.jpg')
+culture = URI.open('https://res.cloudinary.com/dbbmbbgju/image/upload/v1598606107/8qltv4m98uib1dccfhw6wy09mzm0.jpg')
+dancing = URI.open('https://res.cloudinary.com/dbbmbbgju/image/upload/v1598605354/i06pmd809y9jpqnk14881x49b1ji.jpg')
+studying = URI.open('https://res.cloudinary.com/dbbmbbgju/image/upload/v1598521128/studying_rgcpnm.jpg')
+party = URI.open('https://res.cloudinary.com/dbbmbbgju/image/upload/v1598521128/party_t5odfd.jpg')
+food2 = URI.open('https://images.unsplash.com/photo-1458677677220-000ddaa037e3?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=1600')
+spiritual2 = URI.open('https://images.unsplash.com/photo-1554244933-d876deb6b2ff?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=1600')
+political2 = URI.open('https://images.unsplash.com/photo-1551921486-4be2f85f62ed?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=1600')
+sports2 = URI.open('https://images.unsplash.com/photo-1573056020071-9697f575f8d6?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=900&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=1600')
+
 Followship.destroy_all
 Review.destroy_all
 Participation.destroy_all
 Event.destroy_all
 User.destroy_all
 
-score = (0..5).to_a
+score = (1..5).to_a
 
 puts "now generating seeds..."
-
+sleep 0.5
 
 puts "now generating 4 users"
 user_1 = User.new(email: "im_a_test@example.com", first_name: "Maya", last_name: "example", gender: "female", location: "treptow", username: "bee_fan69", password: "123456")
@@ -33,30 +49,81 @@ puts "now saving #{user_4.id}"
 
 
 
-puts "now generating 4 events (one per user)"
+puts "now generating 8 events (two per user, past and future)"
+sleep 0.5
+event_1 = Event.new(category: "food", description: "a very cool event", location: "treptower park", date: (DateTime.now + (score.sample).hours), user: user_1, title: "it's food time (future)")
+event_2 = Event.new(category: "spiritual", description: "a very fun event", location: "babylon berlin", date: (DateTime.now + (score.sample).hours), user: user_2, title: "let's do yoga! (future)")
+event_3 = Event.new(category: "political", description: "a very sexy event", location: "berghain", date: (DateTime.now + (score.sample).hours), user: user_3, title: "clubbing!")
+event_4 = Event.new(category: "sports", description: "a very awesome event", location: "about blank, berlin", date: (DateTime.now + (score.sample).hours), user: user_4, title: "techno fitness (future)")
 
-event_1 = Event.new(category: "food", description: "a very cool event", location: "treptower park", date: "2020-08-25", user: user_1)
-event_2 = Event.new(category: "spiritual", description: "a very cool event", location: "mustafas gemuse", date: "2020-08-25", user: user_2)
-event_3 = Event.new(category: "political", description: "a very cool event", location: "berghain", date: "2020-08-25", user: user_3)
-event_4 = Event.new(category: "sport", description: "a very cool event", location: "about blank, berlin", date: "2020-08-25", user: user_4)
+event_5 = Event.new(category: "culture", description: "Come join our group for a tour through the Pergamon Museum. Bring some time!", location: "Pergamon Museum Berlin", date: (DateTime.now + (score.sample).hours), user: user_1, title: "Art in Pergamon Museum (future)")
+event_6 = Event.new(category: "dancing", description: "We are a small group of enthusiastic salza dancers. Everyone is welcome! No prior knowledge required :)", location: "Volkspark Friedrichshain", date: (DateTime.now + (score.sample).hours), user: user_2, title: "Salza dancing for beginners (future)")
+event_7 = Event.new(category: "studying", description: "I give free tips & tricks how to step up your learning process when it comes to studying.", location: "Staatsbibliothek Berlin", date: (DateTime.now + (score.sample).hours), user: user_3, title: "Tips & Tricks for successfull studying")
+event_8 = Event.new(category: "party", description: "Lets end the day with some pumping beats and some nice & cold beers, 18+ !", location: "Möckernstraße Berlin", date: (DateTime.now + (score.sample).hours), user: user_4, title: "Beers & Beats (future)")
+
+event_9 = Event.new(category: "food", description: "a very scary event", location: "treptower park", date: "2020-08-22", user: user_1, title: "it's food time")
+event_10 = Event.new(category: "spiritual", description: "a very spooky event", location: "alt tegel berlin", date: "2020-08-23", user: user_2, title: "let's do yoga!")
+event_11 = Event.new(category: "political", description: "a very unfortunate event", location: "berghain", date: "2020-08-24", user: user_3, title: "clubbing!")
+event_12 = Event.new(category: "sports", description: "a very arty event", location: "about blank, berlin", date: "2020-08-25", user: user_4, title: "techno fitness")
+
+event_1.photo.attach(io: food, filename: 'food.jpg', content_type: 'image/jpg')
+puts "now addding a photo  #{event_1.id}"
+event_2.photo.attach(io: spiritual, filename: 'spiritual.jpg', content_type: 'image/jpg')
+puts "now addding a photo  #{event_2.id}"
+event_3.photo.attach(io: political, filename: 'political.jpg', content_type: 'image/jpg')
+puts "now addding a photo  #{event_3.id}"
+event_4.photo.attach(io: sports, filename: 'sports.jpg', content_type: 'image/jpg')
+puts "now addding a photo  #{event_4.id}"
+event_5.photo.attach(io: culture, filename: 'culture.jpg', content_type: 'image/jpg')
+puts "now addding a photo #{event_5.id}"
+event_6.photo.attach(io: dancing, filename: 'dancing.jpg', content_type: 'image/jpg')
+puts "now addding a photo #{event_6.id}"
+event_7.photo.attach(io: studying, filename: 'studying.jpg', content_type: 'image/jpg')
+puts "now addding a photo #{event_7.id}"
+event_8.photo.attach(io: party, filename: 'party.jpg', content_type: 'image/jpg')
+puts "now addding a photo #{event_8.id}"
+event_9.photo.attach(io: food2, filename: 'food2.jpg', content_type: 'image/jpg')
+puts "now addding a photo #{event_9.id}"
+event_10.photo.attach(io: spiritual2, filename: 'spiritual2.jpg', content_type: 'image/jpg')
+puts "now addding a photo #{event_10.id}"
+event_11.photo.attach(io: political2, filename: 'political2.jpg', content_type: 'image/jpg')
+puts "now addding a photo #{event_11.id}"
+event_12.photo.attach(io: sports2, filename: 'sports2.jpg', content_type: 'image/jpg')
+puts "now addding a photo #{event_12.id}"
 
 event_1.save
-puts "now saving #{event_1.id}"
+puts "now saving future event #{event_1.id}"
 event_2.save
-puts "now saving #{event_2.id}"
+puts "now saving future event #{event_2.id}"
 event_3.save
-puts "now saving #{event_3.id}"
+puts "now saving future event #{event_3.id}"
 event_4.save
-puts "now saving #{event_4.id}"
+puts "now saving future event #{event_4.id}"
+event_5.save
+puts "now saving past event #{event_5.id}"
+event_6.save
+puts "now saving past event #{event_6.id}"
+event_7.save
+puts "now saving past event #{event_7.id}"
+event_8.save
+puts "now saving past event #{event_8.id}"
+event_9.save
+puts "now saving past event #{event_9.id}"
+event_10.save
+puts "now saving past event #{event_10.id}"
+event_11.save
+puts "now saving past event #{event_11.id}"
+event_12.save
+puts "now saving past event #{event_12.id}"
 
 
 
 puts "now generating 4 participations (one user per event)"
-
-participation_1 = Participation.new(event: event_1, user: user_1)
-participation_2 = Participation.new(event: event_2, user: user_2)
-participation_3 = Participation.new(event: event_3, user: user_3)
-participation_4 = Participation.new(event: event_4, user: user_4)
+sleep 0.5
+participation_1 = Participation.new(event: event_4, user: user_1)
+participation_2 = Participation.new(event: event_5, user: user_2)
+participation_3 = Participation.new(event: event_6, user: user_3)
+participation_4 = Participation.new(event: event_7, user: user_4)
 
 participation_1.save
 puts "now saving #{participation_1.id}"
@@ -70,7 +137,7 @@ puts "now saving #{participation_4.id}"
 
 
 puts "now generating 4 reviews"
-
+sleep 0.5
 review_1 = Review.new(participation: participation_1, score: score.sample, review: "it was an event for sure")
 review_2 = Review.new(participation: participation_2, score: score.sample, review: "it was an event for sure")
 review_3 = Review.new(participation: participation_3, score: score.sample, review: "it was an event for sure")
@@ -88,7 +155,7 @@ puts "now saving #{review_4.id}"
 
 
 puts "now generating 4 followings"
-
+sleep 0.5
 followship_1 = Followship.new(follower: user_1, followee: user_1)
 followship_2 = Followship.new(follower: user_2, followee: user_2)
 followship_3 = Followship.new(follower: user_3, followee: user_3)
